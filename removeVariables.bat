f7170ec3ba73e53a4e5dd92e43b4dee71e2cd133
Rolf <rolf@rolf-mac-pro-2.home>
2020-01-13 22:44:52 +0100

    fix batch file

--- removeVariables.bat
+++ removeVariables.bat
@@ -1,3 +1,16 @@
+@ECHO OFF
+REM Get current folder
+set drive=%~dp0
+set drivep=%drive%
+if #%drive:~-1%# == #\# set drivep=%drive:~0,-1%
+
+REM Remove the Path to the CMake executable
+%drivep%\PackSetup\pathed /REMOVE %ONETHINX_PACK_LOC%/CMake/bin /USER
+REM Remove the Path to the Make executable
+%drivep%\PackSetup\pathed /REMOVE %ONETHINX_PACK_LOC%/mingw-make /USER
+REM Remove the Path to the Compiler
+%drivep%\PackSetup\pathed /REMOVE %ONETHINX_COMPILER_LOC% /USER
+
 REM Remove location of this pack variable
 SET ONETHINX_PACK_LOC=
 REG delete HKCU\Environment /F /V ONETHINX_PACK_LOC
@@ -16,13 +29,6 @@ SET ONETHINX_SVD_LOC=
 REG delete HKCU\Environment /F /V ONETHINX_SVD_LOC
 SET ONETHINX_COMPILER_LOC=
 REG delete HKCU\Environment /F /V ONETHINX_COMPILER_LOC
-
-REM Remove the Path to the CMake executable
-PackSetup\pathed /REMOVE %ONETHINX_PACK_LOC%/CMake/bin /USER
-REM Remove the Path to the Make executable
-PackSetup\pathed /REMOVE %ONETHINX_PACK_LOC%/mingw-make /USER
-REM Remove the Path to the Compiler
-PackSetup\pathed /REMOVE %ONETHINX_COMPILER_LOC% /USER
 
 @ECHO. 
 @ECHO Done, Please restart your machine!
